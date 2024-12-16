3c96bef3b9bb70703e4792b8d7301e7aafce3502
Aleksandr Zhidkov <EastTem@gmail.com>
2024-12-16 12:38:28 +0300

    refactor: services added

--- src/GroupBot/Program.cs
+++ src/GroupBot/Program.cs
@@ -1,87 +1,68 @@
-﻿using GroupBot.Commands;
-using GroupBot.Commands.Abstract;
+﻿using GroupBot.Commands.Abstract;
 using GroupBot.Database;
-using GroupBot.Parser;
 using GroupBot.Requests;
+using GroupBot.Services.Bot;
+using GroupBot.Services.Command;
+using GroupBot.Services.Database;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Telegram.Bot;
-using Telegram.Bot.Polling;
-using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
 
-TelegramBotClient bot = null!;
+namespace GroupBot;
 
-IConfiguration config = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", false, true)
-    .Build();
-
-var botToken = config.GetSection("Tokens")["BotToken"];
-
-if (string.IsNullOrEmpty(botToken))
-  throw new ArgumentException("Bot token environment variable is missing");
-
-using var cts = new CancellationTokenSource();
-
-bot = new TelegramBotClient(botToken, cancellationToken: cts.Token);
-
-if (bot == null)
-  throw new ArgumentException("Bot is null");
-
-var dbPath = config.GetSection("Database")["Path"];
-
-if (string.IsNullOrEmpty(dbPath))
-  throw new ArgumentException("DB Path environment variable is missing");
-
-var sqliteHelper = new DatabaseHelper(dbPath);
-
-var jsonFilePath = config.GetSection("Participants")["Path"];
-
-if (string.IsNullOrEmpty(jsonFilePath))
-  throw new ArgumentException("Participants JSON file path environment variable is missing");
-
-var parser = new ParticipantsParser();
-var participants = parser.Parse(jsonFilePath);
-sqliteHelper.InsertParticipants(participants);
-
-var me = await bot.GetMe(cts.Token);
-
-var requestContainer = new RequestsContainer();
-
-var factory = new CommandFactory();
-
-factory.Register("/start", new StartCommand());
-factory.Register("/addlist", new AddListCommand(sqliteHelper));
-factory.Register("/toend", new ToEndCommand(sqliteHelper));
-factory.Register("/list", new ListCommand(sqliteHelper));
-factory.Register("/lists", new ListsCommand(sqliteHelper));
-factory.Register("/swap", new SwapCommand(requestContainer, sqliteHelper));
-factory.Register("Принять", new SwapAcceptCommand(requestContainer, sqliteHelper));
-factory.Register("Отказаться", new SwapDeclineCommand(requestContainer));
-
-bot.OnError += OnError;
-bot.OnMessage += OnMessage;
-
-Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
-Console.ReadLine();
-
-await cts.CancelAsync();
-
-Task OnError(Exception exception, HandleErrorSource source)
+class Program
 {
-  Console.WriteLine($"Error: {exception.Message}");
-  return Task.CompletedTask;
-}
+    private static void Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
 
-async Task OnMessage(Message msg, UpdateType type)
-{
-  var searchKey = msg.Text?.Split(' ')[0].Replace(" ", string.Empty);
+        var botService = host.Services.GetRequiredService<IBotService>();
+        var commandService = host.Services.GetRequiredService<ICommandService>();
+        var databaseService = host.Services.GetRequiredService<IDatabaseService>();
 
-  if (searchKey == null) return;
+        databaseService.InitializeDatabase();
+        commandService.RegisterCommands();
+        botService.StartBot();
 
-  var command = factory.GetCommand(searchKey);
+        Console.WriteLine("Bot is running... Press Enter to terminate");
+        Console.ReadLine();
 
-  if (command == null) return;
+        host.StopAsync();
+    }
 
-  await command.Execute(msg, bot);
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                 
+                services.AddSingleton<ICommandService, CommandService>();
+                services.AddSingleton<IDatabaseService, DatabaseService>();
+                services.AddSingleton<IBotService, BotService>();
+
+                services.AddSingleton<RequestsContainer>();
+                services.AddSingleton<CommandFactory>();
+
+                services.AddSingleton<DatabaseHelper>(provider =>
+                {
+                    var config = provider.GetRequiredService<IConfiguration>();
+                    var dbPath = config.GetSection("Database")["Path"];
+                    
+                    if (string.IsNullOrEmpty(dbPath))
+                        throw new ArgumentException("DB Path environment variable is missing");
+
+                    return new DatabaseHelper(dbPath);
+                });
+
+                services.AddSingleton<TelegramBotClient>(provider =>
+                {
+                    var config = provider.GetRequiredService<IConfiguration>();
+                    var botToken = config.GetSection("Tokens")["BotToken"];
+                    
+                    if (string.IsNullOrEmpty(botToken))
+                        throw new ArgumentException("Bot token environment variable is missing");
+
+                    return new TelegramBotClient(botToken);
+                });
+            });
 }
---
+++ src/GroupBot/Services/Database/DatabaseService.cs
@@ -0,0 +1,28 @@
+﻿using GroupBot.Database;
+using GroupBot.Parser;
+using Microsoft.Extensions.Configuration;
+
+namespace GroupBot.Services.Database;
+
+public class DatabaseService : IDatabaseService
+{
+    private readonly DatabaseHelper _databaseHelper;
+    private readonly IConfiguration _config;
+
+    public DatabaseService(DatabaseHelper databaseHelper, IConfiguration config)
+    {
+        _databaseHelper = databaseHelper;
+        _config = config;
+    }
+
+    public void InitializeDatabase()
+    {
+        var jsonFilePath = _config.GetSection("Participants")["Path"];
+        if (string.IsNullOrEmpty(jsonFilePath))
+            throw new ArgumentException("Participants JSON file path environment variable is missing");
+
+        var parser = new ParticipantsParser();
+        var participants = parser.Parse(jsonFilePath);
+        _databaseHelper.InsertParticipants(participants);
+    }
+}
